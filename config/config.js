4da290221ad3e1260abcd729822692cb03555849
ArmandBGarcia <xdarcx@hotmail.es>
2023-03-13 03:06:12 -0600

    y

--- config/config.js
+++ config/config.js
@@ -2,12 +2,13 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: "postgres",
+    host: "containers-us-west-42.railway.app",
+    port: 5553,
+    database: "railway",
     dialect: "postgres",
-    logging: false,
+    password: "SYlXHxGG4t6zKlRv6exj",
+    // logging: false,
   },
   test: {
     username: process.env.DB_USER,
